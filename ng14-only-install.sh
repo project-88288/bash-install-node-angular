1c080a42ee47e08996e0d2214beee9ff1459e5f8
project-88288 <chaiya.88288@gmail.com>
2022-09-28 22:14:57 +0700

    new nodejs16-install.sh

--- ng14-only-install.sh
+++ ng14-only-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash # Bash Shell
 
-echo "Remove old nodejs"
-echo "+----------------+"
-sudo apt-get purge --auto-remove nodejs --yes
-
 echo "Check node and npm version"
 echo "+------------------------+"
 node -v
 npm -v
+
+echo "Check angular version"
+echo "+--------------------+"
+ng version
 
 echo "Remove ng-common"
 echo "+--------------+"
@@ -16,20 +16,6 @@ sudo apt-get purge --auto-remove ng-common --yes
 echo "Check angular version"
 echo "+--------------------+"
 ng version
-
-echo "Get node version 16 source code"
-echo "+-----------------------------+"
-wget https://deb.nodesource.com/setup_16.x -O ./bash-script-nodejs-setup_16.x.sh
-
-echo "Setup nodejs version 16 from source code"
-echo "+---------------------------------------+"
-sudo -E bash ./bash-script-nodejs-setup_16.x.sh
-sudo apt-get install -y nodejs
-
-echo "Check node and npm version"
-echo "+------------------------+"
-node -v
-npm -v
 
 echo "Install angular"
 echo "+----------------+"
--- nodejs16-install.sh
+++ nodejs16-install.sh
@@ -9,14 +9,6 @@ echo "+------------------------+"
 node -v
 npm -v
 
-echo "Remove ng-common"
-echo "+--------------+"
-sudo apt-get purge --auto-remove ng-common --yes
-
-echo "Check angular version"
-echo "+--------------------+"
-ng version
-
 echo "Get node version 16 source code"
 echo "+-----------------------------+"
 wget https://deb.nodesource.com/setup_16.x -O ./bash-script-nodejs-setup_16.x.sh
@@ -30,13 +22,6 @@ echo "Check node and npm version"
 echo "+------------------------+"
 node -v
 npm -v
-
-echo "Install angular"
-echo "+----------------+"
-sudo npm install -g @angular/cli@14 --yes --yes --yes
-echo "Check angular version"
-echo "+--------------------+"
-ng version
 
 echo "Autoremove.."
 echo "+-----------------------+"
